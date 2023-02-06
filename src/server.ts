1ce91e82eca0246cbe376a7a66313970c5debed3
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-06 20:58:15 -0300

    config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hello', () => {
-  return 'Hello World';
-});
+  return 'Hello World'
+})
 
 app.listen({ port: 3333 }).then(() => {
-  console.log(`🚀 HTTP Server Running on localhost:3333`);
-});
+  console.log(`🚀 HTTP Server Running`)
+})
