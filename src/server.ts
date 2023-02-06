6319b909697741e192cca6e85da658c4405414ab
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-06 20:35:13 -0300

    create project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', () => {
+  return 'Hello World';
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log(`🚀 HTTP Server Running on localhost:3333`);
+});
