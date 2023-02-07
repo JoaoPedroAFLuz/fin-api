24ac3bd8fc998807569dddfbb134542e46daae5f
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-06 22:23:26 -0300

    feat: list and create transactions

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
+
 import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+app.get('/transactions', async () => {
+  const transaction = await knex('transactions').select('*')
 
-  return tables
+  return transaction
+})
+
+app.post('/transactions', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: `Transação ${Math.floor(Math.random() * 100)}`,
+      amount: (Math.random() * 1000).toFixed(2),
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
