1b4b29dd295a395d13f8b749657072aaf3a9ec57
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-06 23:58:13 -0300

    feat: trate env varibles with zod

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import crypto from 'node:crypto'
 
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -23,6 +24,6 @@ app.post('/transactions', async () => {
   return transaction
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log(`🚀 HTTP Server Running`)
 })
