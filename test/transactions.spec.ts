b35558e1f86cee384ea39cc7748a0812c0739cb4
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-02-08 19:21:29 -0300

    feat: 2e2 test - get a specific transaction

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -42,6 +42,38 @@ describe('Transactions routes', () => {
     ])
   })
 
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionsResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionsResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        id: transactionId,
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
   it('should  be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
